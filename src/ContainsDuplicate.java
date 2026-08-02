98aa600fe6d0ce415ed52cb73cd0c8c4d330c13e
Turaj <h_turaj@yahoo.com>
2026-08-02 22:52:46 +0400

    contains duplicate

---
+++ src/ContainsDuplicate.java
@@ -0,0 +1,21 @@
+import java.util.Formattable;
+import java.util.HashSet;
+
+public class ContainsDuplicate {
+    public boolean containsDuplicate(int [] nums){
+        HashSet<Integer> set = new HashSet<>();
+        for (int num:nums){
+            if (set.contains(num)){
+                return  true;
+            }
+            set.add(num);
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        ContainsDuplicate cd = new ContainsDuplicate();
+        int[] nums = {1,3,5,4,3,6,2};
+        System.out.println(cd.containsDuplicate(nums));
+    }
+}
