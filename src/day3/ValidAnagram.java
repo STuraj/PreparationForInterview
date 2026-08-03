703cf5bfc80bbd3be8f45a747ddc64a4c46194bc
Turaj <h_turaj@yahoo.com>
2026-08-03 23:33:08 +0400

    Valid anagram English letter

--- src/day3/ValidAnagram.java
+++ src/day3/ValidAnagram.java
@@ -4,27 +4,27 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ValidAnagram {
-public static boolean isAnagram(String s, String t){
-    if (s.length()!= t.length()){
-        return false;
-    }
-    Map<Character, Integer> map = new HashMap<>();
-    //Count characters in s
-    for (char c : s.toCharArray()){
-        map.put(c, map.getOrDefault(c,0) + 1);
-    }
-    //Remove characters using t
-    for (char c : t.toCharArray()){
-        if (!map.containsKey(c)){
+    public static boolean isAnagram(String s, String t){
+        if (s.length()!= t.length()){
             return false;
         }
-        map.put(c, map.get(c)-1);
-        if (map.get(c)<0){
-            return false;
+        Map<Character, Integer> map = new HashMap<>();
+        //Count characters in s
+        for (char c : s.toCharArray()){
+            map.put(c, map.getOrDefault(c,0) + 1);
         }
+        //Remove characters using t
+        for (char c : t.toCharArray()){
+            if (!map.containsKey(c)){
+                return false;
+            }
+            map.put(c, map.get(c)-1);
+            if (map.get(c)<0){
+                return false;
+            }
+        }
+        return true;
     }
-    return true;
-}
 
     public static void main(String[] args) {
         ValidAnagram va= new ValidAnagram();
---
+++ src/day3/ValidAnagramEL.java
@@ -0,0 +1,27 @@
+package day3;
+
+public class ValidAnagramEL {
+    public boolean isAnagram(String s, String t){
+        if (s.length()!=t.length()){
+            return false;
+        }
+        int[] count = new int[26];
+        for (int i=0; i<s.length(); i++){
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+                    }
+        for (int num : count){
+            if (num!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        ValidAnagramEL va = new ValidAnagramEL();
+        String s= "mobile";
+        String t = "ilebom";
+        System.out.println(va.isAnagram(s,t));
+    }
+}
