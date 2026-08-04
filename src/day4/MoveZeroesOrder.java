a98be3861fd1aa0f83eb823af98b2ef08e0d3cb0
Turaj <h_turaj@yahoo.com>
2026-08-04 18:45:33 +0400

    Mive zeroes order

---
+++ src/day4/MoveZeroesOrder.java
@@ -0,0 +1,25 @@
+package day4;
+
+public class MoveZeroesOrder {
+    public static void moveZeroes(int [] nums){
+        int index =0;
+        for (int i=0; i<nums.length; i++){
+            if (nums[i]!=0){
+                nums[index]=nums[i];
+                index++;
+            }
+        }
+        while (index<nums.length){
+            nums[index]=0;
+            index++;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0,1,2,0,4,0,3};
+        moveZeroes(nums);
+        for (int num : nums){
+            System.out.print(num + " ");
+        }
+    }
+}
