c68c825b3dc3e86adb3901a254616007c414a298
Turaj <h_turaj@yahoo.com>
2026-08-09 22:24:11 +0400

    Longest common prefix

---
+++ src/day8/LongestCommonPrefix.java
@@ -0,0 +1,24 @@
+package day8;
+
+public class LongestCommonPrefix {
+    public static String longestCommonPrefix(String [] str){
+        if (str==null || str.length==0){
+            return "";
+        }
+        String prefix= str[0];
+        for (int i=1; i<str.length; i++){
+            while (!str[i].startsWith(prefix)){
+                prefix= prefix.substring(0, prefix.length()-1);
+                if (prefix.isEmpty()){
+                    return "";
+                }
+            }
+        }
+        return prefix;
+    }
+
+    public static void main(String[] args) {
+        String [] words = {"flower", "flight", "flow"};
+        System.out.println(longestCommonPrefix(words));
+    }
+}
