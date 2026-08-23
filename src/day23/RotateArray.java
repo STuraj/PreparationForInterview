5dabe615ca9c4fd23d204ac3ff303a4920bafc10
Turaj <h_turaj@yahoo.com>
2026-08-23 23:31:11 +0400

    Rotate Array

---
+++ src/day23/RotateArray.java
@@ -0,0 +1,28 @@
+package day23;
+
+public class RotateArray {
+    public static  void  rotateArray(int [] nums, int k){
+        int n= nums.length;
+        k=k%n;
+        reverse(nums,0,n-1);
+        reverse(nums, 0, k-1);
+        reverse(nums, k, n-1);
+    }
+    public static void reverse(int[] nums, int left, int right){
+        while (left<right){
+            int temp = nums[left];
+            nums[left]= nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int [] nums = {1,2,3,4,5,6,7};
+        rotateArray(nums, 3);
+        for (int num : nums){
+            System.out.print(num + " ");
+        }
+    }
+}
